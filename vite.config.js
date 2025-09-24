bc2a3576ca8da8069d237f1ad87b4a4352f723ec
KUMAR <kumar@KUMARs-MacBook-Air.local>
2025-09-24 22:33:13 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{port:5174}
+    plugins: [react()],
+    base: '/admin/', // <-- This line is crucial for Tomcat subdirectory
+    server: { port: 5174 }
 })
